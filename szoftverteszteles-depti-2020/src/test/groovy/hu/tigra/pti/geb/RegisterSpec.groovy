ffda061f8af3b7d1c22e40c4775dfd2939032137
szabofeco98 <szabofeco98@gmail.com>
2020-04-06 17:12:43 +0200

    2. hazi regisztrációs update

--- szoftverteszteles-depti-2020/src/test/groovy/hu/tigra/pti/geb/RegisterSpec.groovy
+++ szoftverteszteles-depti-2020/src/test/groovy/hu/tigra/pti/geb/RegisterSpec.groovy
@@ -10,7 +10,7 @@ class RegisterSpec extends BaseSpec {
         def loginPage = to LoginPage
 
         when: 'Kitöltöm a "CREATE AN ACCOUNT" blokkban az "Email address" mezőt egy még nem regisztrált email címmel és a "Create an account" funkciógombra kattintok.'
-        loginPage.registerEmailAddress = 'dghgdfhdfg@email.hu'
+        loginPage.registerEmailAddress = 'sss@email.hu'
         loginPage.registerButton.click()
 
         then: 'Megjelenik a "Create an account" űrlap.'
@@ -35,14 +35,16 @@ class RegisterSpec extends BaseSpec {
         registerPage.addressLastName = 'Mekk'
         registerPage.addressCompany = 'gyuszi és fia bt.'
         registerPage.address1 = '4821 Debrecen kis utca 32'
-     //   registerPage.address2 = '1 emelet 5'
+        //registerPage.address2 = '4'
         registerPage.city = 'Debrecen'
         registerPage.state = 'Alaska'
         registerPage.postcode = '00000'
         registerPage.country = 'United States'
         registerPage.phone = '06401234567'
         registerPage.phone_mobile = '06401234567'
+        registerPage.alias="gyuri"
         registerPage.registerButton.click()
+
 
         then: 'Megjelenik a felhasználó adatai felület: “My account”.'
         def myAccountPage = waitFor { at hu.tigra.pti.geb.page.MyAccountPage }
